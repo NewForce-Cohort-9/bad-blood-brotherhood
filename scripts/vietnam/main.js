6e9bba7d865186f0d41d914bb839e75f4172c59d
Meowph <bpardue33@gmail.com>
2024-04-23 10:19:37 -0400

    good

--- scripts/vietnam/main.js
+++
@@ -1,7 +0,0 @@
-import { getVDataInfo } from "./database.js"
-const alldata = getFish()
-for (const fish of allFish) {
-    console.log(fish)
-}
-// Import the FishList function from the correct module
-import {fishlist} from './scripts/fishList.js'
